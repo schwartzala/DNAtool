3d4d78e51e807cb6387a9c5581ae0b4f8635f6bf
Alan Schwartz <schwartzala@my.commnet.edu>
2016-05-12 15:42:22 -0400

    Significant changes to the program's functions. Still some work to do.

--- driver.py
+++ driver.py
@@ -1,45 +1,29 @@
-def file_to_dna(file):
-    sequence = ""
-    with open(file) as f:
-        for line in f:
-            line = line.replace('\n', '')
-            sequence = sequence + line
-    if "U" in sequence:
-        strand_type = "RNA"
+# help prompt that shows command usage.
+def print_help():
+    print "[L]oad: Import a DNA or RNA sequence from file"
+    print "[H]elp: Prompt usage script"
+    print "[E]xit: Exit the program\n"
+
+# run flag is used to see if the user wishes to continue using the application.
+run = True
+# show help prompt at the beginning for new users
+print_help()
+while run:
+    # Request user to input a command.
+    command = str(raw_input("Please enter a command: [L]oad / [H]elp / [E]xit\n")).lower()
+    # If "e" or "exit", exit the program.
+    if command == "e" or command == "exit":
+        print "See you later! o/"
+        run = False
     else:
-        strand_type = "DNA"
-    return [sequence, strand_type]
-
-
-def rna_to_dna(sequence):
-    sequence = sequence.replace("A", "T")
-    sequence = sequence.replace("U", "A")
-    sequence = sequence.replace("C", " ")
-    sequence = sequence.replace("G", "C")
-    sequence = sequence.replace(" ", "G")
-    return sequence
-
-
-def read_dna(sequence):
-    return sequence
-
-
-dna_file_path = "dna.txt"
-dna = file_to_dna(dna_file_path)
-print dna[0]
-print dna[1]
-if dna[1] == "RNA":
-    dna[0] = rna_to_dna(dna[0])
-    dna[1] = "DNA"
-print dna[0]
-print dna[1]
-
-rna_file_path = "rna.txt"
-rna = file_to_dna(rna_file_path)
-print rna[0]
-print rna[1]
-if rna[1] == "RNA":
-    rna[0] = rna_to_dna(rna[0])
-    rna[1] = "DNA"
-print rna[0]
-print rna[1]
+        # If "h" or "help", show help prompt.
+        if command == "h" or command == "help":
+            print_help()
+        else:
+            # If "l" or "load", prompt user to entire file location.
+            if command == "l" or command == "load":
+                fileLoc = str(raw_input("Please enter the location of your DNA / RNA sequence file: "))
+            # If command is not valid, inform the user and request another input.
+            else:
+                print command, "not a valid command! Please use one of the following:"
+                print_help()
---
+++ file_handler.py
@@ -0,0 +1,18 @@
+# check_file method checks to see if file location is valid.
+def check_file(file_loc):
+    try:
+        open(file_loc)
+    except IOError:
+        print "\"" + file_loc + "\" is not a valid file location"
+        return False
+    return True
+
+
+# concat_lines method takes all lines in a file and concatenates them into a single String.
+def concat_lines(file_loc):
+    big_line = ""
+    with open(file_loc) as f:
+        for line in f:
+            line = line.replace('\n', '')
+            big_line = big_line + line
+    return big_line
